30a19871bbd728c8e1e22bd9418c9b5025d772b4
fernandovldrs <fernandovldrs@gmail.com>
2025-03-02 10:05:49 +0800

    add description

--- solve_diophantine.py
+++ solve_diophantine.py
@@ -1,6 +1,15 @@
 import itertools
 import pickle
 
+###########################################################################
+##                                                                       ##
+## This script finds solutions to a given diophantine equation in the    ##
+## integer domain with given cutoffs. This is used to calculate          ##
+## the effective coupling factor between qubit and resonator during      ##
+## parametric modulation.                                                ##
+##                                                                       ##
+###########################################################################
+
 k_cutoff = 6
 n_cutoff = 6
 N = -2
