44150c9ae1419f8a4003186dc6ed1d43c316321e
fernandovldrs@gmail.com <your.email@example.com>
2025-04-01 13:57:16 +0800

    rename, comment and reorganize files

--- helper_fns.py
+++ helper_fns.py
@@ -2,6 +2,8 @@ import qutip
 import numpy as np
 from scipy.integrate import quad
 import matplotlib.pyplot as plt
+from scipy.special import jv
+import pickle
 
 def detuned_drive(A, f0, phi, t0, length):
     def envelope(t, args):
@@ -123,6 +125,105 @@ def create_qutip_initial_state(res_trunc, aux_trunc, transmon_trunc):
     return qutip.tensor(qutip.basis(res_trunc, 0), 
                                 qutip.basis(aux_trunc, 0), 
                                 qutip.basis(transmon_trunc, 0))
+
+
+def calc_flux_noise(f0, d, p, phi_dc, A_flux1, A_flux2, theta):
+    ## Calculates the susceptibility to noise of a qubit
+    ## modulated with a two-tone flux pulse.
+
+    phi_ac = np.sqrt(A_flux1**2 + A_flux2**2)
+    alpha = np.arcsin(A_flux2/phi_ac)
+
+    # print(phi_ac)
+    # print(alpha/2/np.pi)
+    # print(theta/2/np.pi)
+
+    # Get the fourier series of the flux curve
+    freq_curve = lambda flux: f0*f_scale(flux, d)
+    T, N = 1, 10
+    freq_coeff = calc_fourier_cosine_series(freq_curve, T, N, plot = False)
+
+    def analytical_f_avg(phi_dc, phi_ac, alpha, theta):
+        f_bar = 0
+        m_cutoff = 20
+        n_cutoff = N
+        for m in range(m_cutoff+1):
+            for n in range(0, n_cutoff+1):
+                s = 1
+                s *= np.cos(m*theta)
+                s *= freq_coeff[n]
+                s *= np.cos(n*2*np.pi*phi_dc + (p + 1)*m*np.pi/2) 
+                s *= (2 - (1 if m == 0 else 0) )
+                s *= jv(p*m, n*2*np.pi*phi_ac*np.cos(alpha))
+                s *= jv(m, n*2*np.pi*phi_ac*np.sin(alpha))
+                f_bar += s
+
+        return f_bar
+
+    def dfavg_dphi_dc(phi_dc, phi_ac, alpha, theta):
+
+        f = lambda x: analytical_f_avg(x, phi_ac, alpha, theta)
+
+        def central_difference(f, x, h):
+            return (f(x + h) - f(x - h)) / (2 * h)
+        
+        return central_difference(f, phi_dc, h = 1/5000)
+
+    def dfavg_dphi_ac(phi_dc, phi_ac, alpha, theta):
+
+        f = lambda x: analytical_f_avg(phi_dc, x, alpha, theta)
+
+        def central_difference(f, x, h):
+            return (f(x + h) - f(x - h)) / (2 * h)
+        
+        return central_difference(f, phi_ac, h = 1/5000)
+
+    def decoherence(phi_dc, phi_ac, alpha, theta):
+        dphi_dc = dfavg_dphi_dc(phi_dc, phi_ac, alpha, theta)
+        dphi_ac = dfavg_dphi_ac(phi_dc, phi_ac, alpha, theta)
+        Adc = 33e-6
+        Aac = 33e-6 
+        return 2*np.pi*3*np.sqrt(Adc**2*dphi_dc**2 + Aac**2*dphi_ac**2)
+    
+    return decoherence(phi_dc, phi_ac, alpha, theta)
+
+def calculate_geff(A_flux1, A_flux2, f0, d, p, w_flux_base, N=0, phase = 0):
+    ## Calculates the effective coupling factor for a given sideband of a parametrically
+    ## modulated qubit.
+
+    def flux_modulation(t, A_flux1, A_flux2, d):
+        flux = A_flux1 * np.cos(w_flux_base * t) + A_flux2 * np.cos(w_flux_base * p * t + phase)
+        return f_scale(flux, d) * f0
+
+    # Compute Fourier series coefficients of the frequency
+    T = 2*np.pi/w_flux_base  # Total period
+    f = lambda t: flux_modulation(t, A_flux1, A_flux2, d)
+    num_coeffs = 30
+    coeffs, thetas = calc_fourier_series(f, T, num_coeffs, plot = False)
+    wq_k = [2*np.pi*c for c in coeffs]
+
+    # Load selected geff combinations
+    with open(f"diophantine_eq_solutions\\selected_combinations_N{N}.pkl", "rb") as f:
+        selected_combinations = pickle.load(f)
+    n_cutoff = max([max(comb) for comb in selected_combinations])
+    k_cutoff = len(selected_combinations[0])
+
+    # Calculate g_eff
+    g_eff = 0
+    g_eff_factor_list = []
+    for comb in selected_combinations:
+        g_eff_factor = 1
+        for k in range(1, k_cutoff+1):
+            J_arg = wq_k[k]/w_flux_base/k
+            J = jv(comb[k-1], J_arg)
+            J_phase_factor = np.exp(1j*comb[k-1]*thetas[k])
+            g_eff_factor *= J*J_phase_factor
+        g_eff += g_eff_factor
+        g_eff_factor_list.append(g_eff_factor)
+
+    return np.abs(g_eff)
+
+
 
 class transmon_simple:
     
